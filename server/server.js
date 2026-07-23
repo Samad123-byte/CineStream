397dc3c3b71546e9f55ac255bbbb0a762355c093
Abdul Samad <abdulsammadk5@gmail.com>
2026-07-23 20:43:59 +0500

    done

--- server/server.js
+++ server/server.js
@@ -21,7 +21,15 @@ app.use(express.json());
 
 app.use(cookieParser());
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://cine-stream-pi-two.vercel.app"
+    ],
+    credentials: true,
+  })
+);
 
 app.use("/api/auth", authRoutes)
 app.use("/api/users", userRoutes)
